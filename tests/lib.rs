455177c3affa6030c60f580a98cdea6cf6d6ea60
Carl Colglazier <carl@carlcolglazier.com>
2015-05-15 09:11:29 -0400

    Move API tests to 'tests' directory.

---
+++ tests/lib.rs
@@ -0,0 +1,43 @@
+extern crate johnmark;
+
+use johnmark::convert;
+
+#[test]
+fn header_paragraph() {
+    assert_eq!("<h2>Header</h2>", convert("## Header"));
+    assert_eq!("<h1>Header</h1>", convert("Header\n==="));
+    assert_eq!("<h2>Header</h2>", convert("Header\n---"));
+    assert_eq!("<h1>Header</h1><h1>Header</h1>", convert("Header\n===\n\n# Header"));
+    assert_eq!("<p>Content</p>", convert("Content"));
+    assert_eq!("<h5>Header</h5><p>Content</p>", convert("##### Header\n\nContent"));
+}
+
+#[test]
+fn blockquote_paragraph() {
+    assert_eq!("<blockquote><p>Quote</p></blockquote>", convert(">Quote"))
+}
+
+#[test]
+fn emphasis() {
+    assert_eq!("<p><strong>bold</strong></p>", convert("**bold**"));
+    assert_eq!("<p><em>bold</em></p>", convert("*bold*"));
+    assert_eq!("<p><strong><em>bold</em></strong></p>", convert("***bold***"));
+    assert_eq!("<p>*<strong><em>bold</em></strong>*</p>", convert("****bold****"));
+
+    // Probably will not happen, but you never know!
+    assert_eq!("<p>****<strong><em>bold</em></strong>****</p>", convert("*******bold*******"));
+
+    // Unclosed
+    assert_eq!("<p>**bold*</p>", convert("**bold*"));
+}
+
+#[test]
+fn code_block() {
+    assert_eq!("<p>Some <code>*code*</code></p>", convert("Some `*code*`"));
+}
+
+#[test]
+fn inline_html() {
+    assert_eq!("<section><p>HTML</p></section>", convert("<section><p>HTML</p></section>"));
+    assert_eq!("<p><section HTML</p>", convert("<section HTML"));
+}
